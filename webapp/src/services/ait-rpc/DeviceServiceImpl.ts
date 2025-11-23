4ae343f82836741255a42fc68b30968dbde958d0
Kwanjoong Lee <leekwanjung@gmail.com>
2025-11-23 14:36:04 +0900

    fix: add additional safe area space

--- webapp/src/services/ait-rpc/DeviceServiceImpl.ts
+++ webapp/src/services/ait-rpc/DeviceServiceImpl.ts
@@ -1,5 +1,6 @@
 import { getSafeAreaInsets } from "@apps-in-toss/web-framework";
 import { DeviceServiceBase } from "../../generated/DeviceService/ait_device_DeviceServiceBase";
+import { getPlatformOS } from "@apps-in-toss/web-framework";
 import type {
   GetSafeAreaInsetsRequest,
   GetSafeAreaInsetsResponse,
@@ -17,14 +18,24 @@ export class DeviceServiceImpl extends DeviceServiceBase {
     console.log(`[DeviceService] Safe Area Insets (CSS):`, insets);
     console.log(`[DeviceService] Device Pixel Ratio:`, ratio);
 
+    const platformOS = getPlatformOS();
+
+    let topValue = insets.top ?? 0;
+    if (platformOS === "ios") {
+      topValue = topValue + 30;
+    } else if (platformOS === "android") {
+      topValue = topValue + 40;
+    }
+
     const response: GetSafeAreaInsetsResponse = {
-      top: (insets.top ?? 0) * ratio,
+      top: topValue * ratio,
       bottom: (insets.bottom ?? 0) * ratio,
       left: (insets.left ?? 0) * ratio,
       right: (insets.right ?? 0) * ratio,
     };
 
     console.log(`[DeviceService] Safe Area Insets (Device Pixels):`, response);
+
 
     return response;
   }
